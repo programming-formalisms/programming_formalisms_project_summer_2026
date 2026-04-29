72311eea4697b37068f3dffe51b432ea384619ff
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-04-29 12:00:58 +0200

    Fix all tests

--- src/weather/analysis.py
+++ src/weather/analysis.py
@@ -3,8 +3,10 @@
 
 def do_analysis():
     """Do the analysis."""
-    with open("figure.png") as figure_file:
+    open("figure.png", "a").close() # Create the file if it does not exist
+    with open("figure.png", "w") as figure_file:
         figure_file.write("Stub for an image")
 
-    with open("statistics_results.txt") as stats_file:
+    open("statistics_results.txt", "a").close() # Create the file if it does not exist
+    with open("statistics_results.txt", "w") as stats_file:
         stats_file.write("Stub for a statistics file")
