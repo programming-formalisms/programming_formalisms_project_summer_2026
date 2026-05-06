f76fb4fb28ad8eac447db6179405cda35592b9b2
mnyt-aqw <68369485+mnyt-aqw@users.noreply.github.com>
2026-05-06 10:21:10 +0200

    Added test ot see of no input was given to function

--- learners/Marcus_and_Stefan.py
+++ learners/Marcus_and_Stefan.py
@@ -1,13 +1,14 @@
-def is_zero(x):
+def is_zero(x=None):
     """Determines if the input is one integer that is zero"""
+    if x == None:
+        raise TypeError("No value assinged")
     if not isinstance(x, int):
         raise TypeError("'x' must be of type int")
     if x == 0:
         return True
     return False
 
-
-
+assert is_zero()
 
 
 # Returns True if the input is even
