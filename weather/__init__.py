cebfbe73017c514de698a63c4b119790e980388e
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-05-06 07:38:31 +0200

    Fix main

--- weather/__init__.py
+++ weather/__init__.py
@@ -1,10 +1,1 @@
 """Code for the 'weather' package."""
-"""Code for the 'weather' package."""
-
-def do_experiment():
-    # Read the data
-    # Do the statistics
-    # Save the statistics results to file
-    # Create the figure
-    # Save the figure to file
-    pass
