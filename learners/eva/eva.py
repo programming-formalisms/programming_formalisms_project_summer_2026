76cbbdd8a3f24305a42c4fca2545ee114ef2827d
Eva Krämer <eva.kramer@umu.se>
2026-05-06 10:00:10 +0200

    is_zero throws error if wrong input type

--- learners/eva/eva.py
+++ learners/eva/eva.py
@@ -1,6 +1,9 @@
 def is_zero(num):
     """Check if input is zero. Return True if zero, False otherwise, Raises error if not a number.
     """
+    if not isinstance(num, (int, float)):
+        raise TypeError("Input must be of type int or float")
+    
     if num == 0:
         return True
     else:
@@ -9,3 +12,10 @@ def is_zero(num):
 assert is_zero.__doc__
 assert is_zero(0)
 assert not is_zero(10)
+
+has_thrown = False
+try:
+    is_zero("nonsense")
+except:
+    has_thrown = True
+assert has_thrown
