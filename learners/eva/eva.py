0862582c720829b21c0615b8eff90be5a59acdbd
Eva Krämer <eva.kramer@umu.se>
2026-05-06 09:54:39 +0200

    Changed function to return False if input in not zero

--- learners/eva/eva.py
+++ learners/eva/eva.py
@@ -1,7 +1,11 @@
 def is_zero(num):
     """Check if input is zero. Return True if zero, False otherwise, Raises error if not a number.
     """
-    return True
+    if num == 0:
+        return True
+    else:
+        return False
 
 assert is_zero.__doc__
 assert is_zero(0)
+assert not is_zero(10)
