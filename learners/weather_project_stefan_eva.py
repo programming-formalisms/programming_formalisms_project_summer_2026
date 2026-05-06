d5ee7a0cbf52709a855eef829c990bd10784fc7a
Eva Krämer <eva.kramer@umu.se>
2026-05-06 14:54:30 +0200

    file_exists passes True if file exsits

--- learners/weather_project_stefan_eva.py
+++ learners/weather_project_stefan_eva.py
@@ -1,9 +1,11 @@
 # Script to analyze weather data from Uppsala
 
-def file_exists():
+def file_exists(s):
     '''
     Check if file exists
     '''
+    import os
+    return os.path.exists(s)
 
 def do_analysis():
     '''
@@ -21,3 +23,4 @@ def do_analysis():
 assert do_analysis.__doc__
 assert file_exists.__doc__
 assert not file_exists('sokfsdofjsd')
+assert file_exists(r"C:\Users\EVAK\OneDrive - Umeå universitet\Courses\2026_ProgrammingFormalisms\programming_formalisms_project_summer_2026\learners\test.md")
