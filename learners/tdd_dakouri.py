713893a7e6437a8ab6fa7b8e3528e7442f680a97
Dakouri Kobri <dakouri.m.kobri@gmail.com>
2026-05-06 10:24:31 +0200

    is_even returns false when input is not even

--- learners/tdd_dakouri.py
+++ learners/tdd_dakouri.py
@@ -2,9 +2,10 @@ print()
 
 def is_even(n):
     """Determines if input is an even integer"""
-    return True
+    return n % 2 == 0
 
 assert is_even.__doc__
 assert is_even(2)
+assert not is_even(3)
 
 print()
