1bcc32d9c89cf547d212bce3c8ec07a831d73cb1
Federico Agostini <federico.agostini@scilifelab.se>
2026-05-06 09:51:40 +0200

    is_zero returns false when argument is 1

--- learners/federico_tdd.py
+++ learners/federico_tdd.py
@@ -1,7 +1,11 @@
 def is_zero(n):
     """Function to test if the argument is zero."""
-    return True
+    if n == 0:
+        return True
+    else:
+        return False
 
 
 assert is_zero.__doc__
 assert is_zero(0)
+assert not is_zero(1)
