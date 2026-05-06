ac088d5e243cefe7bbc8951f31ff219be1557fd0
stane <stanek.petr.19@gmail.com>
2026-05-06 10:22:08 +0200

    Also tests if input is float or int

--- learners/stane/my_tdd.py
+++ learners/stane/my_tdd.py
@@ -1,16 +1,24 @@
 # Exercise 5.1
 
-def is_zero(testnum: int):
+def is_zero(testnum: int) -> bool:
     """
     This function accepts one argument that should be a float or an interger
 
     Returns True if input is 0,
     Returns False if input is not 0,
-    Rarises TypeError is input is not flat or int
+    Rarises TypeError is input is not float or int
 
     """
+    if not isinstance(testnum, (float,int)):
+        raise TypeError("Input must be of type int or float")
     return True if testnum == 0 else False
 
 assert is_zero(0).__doc__
 assert is_zero(0) 
 assert not is_zero(1)
+has_thrown = False
+try:
+    is_zero('somestring')
+except:
+    has_thrown = True
+assert has_thrown
