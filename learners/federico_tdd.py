54f81a9bec4efa306d0df9645df8d2309bb546d6
Federico Agostini <federico.agostini@scilifelab.se>
2026-05-06 09:55:48 +0200

    is_zero checks that the argument is an integer

--- learners/federico_tdd.py
+++ learners/federico_tdd.py
@@ -1,5 +1,7 @@
 def is_zero(n):
     """Function to test if the argument is zero."""
+    if not isinstance(n, int):
+        raise TypeError("'n' is not an integer")
     if n == 0:
         return True
     else:
@@ -9,3 +11,10 @@ def is_zero(n):
 assert is_zero.__doc__
 assert is_zero(0)
 assert not is_zero(1)
+
+is_thrown = False
+try:
+    is_zero("zero")
+except:
+    is_thrown = True
+assert is_thrown
