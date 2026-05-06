443e787ba31a4ce362f451a13ba07defb9dd5d3d
felix-falk <107868405+felix-falk@users.noreply.github.com>
2026-05-06 11:25:40 +0200

    Revised the read_file function to read the weather dat file.

--- learners/my_assert_felix.py
+++ learners/my_assert_felix.py
@@ -1,48 +1,8 @@
-#def divide_by(numerator, denominator):
-#    assert denominator != 0.0
-#    assert isinstance(numerator, float)
-#    assert isinstance(denominator, float)
-#    return (numerator / denominator)
+#import weather
 
-#divide_by(3, 4)
+# weather.do_experiment()
 
-def read_file(filename):
-    import os
-    assert os.path.isfile(filename)
-    assert os.access(filename, os.R_OK)
-
-    file = open(filename, "r")
-    content = file.read()
-    file.close()
-    return content
-
-read_file("/Users/felix.falk/programming_formalisms_project_summer_2026/learners/fairytale_felix_claudia.md")
-
-print(read_file("/Users/felix.falk/programming_formalisms_project_summer_2026/learners/fairytale_felix_claudia.md"))
-
-read_file("/Users/felix.falk/programming_formalisms_project_summer_2026/learners/fqwgqgwqggqe.md") 
-
-def read_non_empty_file(filename):
-    import os
-    assert os.path.isfile(filename)
-    assert os.access(filename, os.R_OK)
-    
-    file = open(filename, "r")
-    content = file.read()
-    
-    assert content != NULL
-
-    file.close()
-    return content
-
-import weather
-import os
-assert os.path.isfile(filename) # Is the filename correct? 
-assert os.access(filename, os.R_OK) # Is the file readable?
-
-weather.do_experiment()
-
-def do_experiment():
+def do_experiment(dat_filename):
     import os
     assert os.path.isfile(filename) # Is the filename correct? 
     assert os.access(filename, os.R_OK) # Is the file readable?
@@ -56,3 +16,20 @@ def do_experiment():
   # Save the figure to file
     assert file_exists("figure.png") # Is the output figure created? 
     assert file_exists("statistics.txt") # Is the output statistic file created? 
+
+path = "/Users/felixfalk/programming_formalisms_project_summer_2026/data/uppsala_tm_1722-2022.dat"
+
+def read_file(filename):
+    import os
+    assert os.path.isfile(filename)
+    assert os.path.getsize(filename)
+    assert os.access(filename, os.R_OK)
+    
+
+    file = open(filename, "r")
+    content = file.read()
+
+    file.close()
+    return content
+
+read_file(path)
