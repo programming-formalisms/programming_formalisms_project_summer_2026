a770d2c9e158c1e48e5b6e6376b08a9f826fefe5
stane <stanek.petr.19@gmail.com>
2026-05-06 15:34:13 +0200

    implementing is_weatherData test function

--- learners/PetrChaitali_weather/weather.py
+++ learners/PetrChaitali_weather/weather.py
@@ -4,11 +4,29 @@ import sys
 import numpy as np
 import pandas as pd
 
+
+# column       data
+# 1-3          Year, month, day
+# 4            Daily average temperature according to observations. 
+#                Unit: °C
+# 5            Daily average temperatures corrected for the urban effect.
+#              NOTE: The urban effects are here updated after 2000 and larger than
+#              previously used due to an increased urban heat island at the site.
+# 6            Data id no. meaning data from:
+#                1=Uppsala, 2=Risinge, 3=Betna, 4=Linköping, 5=Stockholm, 6=Interpolated
+
 def table_to_df(file):
     """ This function reads in the table. """
     data= pd.read_csv(file)
     return data
 
+def is_weatherData(df):
+    """
+    Tests if columns are as follows: Year[int], Month[int], Day[int], Temperature[float], Temp_corrected[float], Location[int] 1-6
+
+    Returns true if the the format is correct, otherwise returns False (and column name that fails)
+    """
+    return True
 
 def is_dataframe(df):
     """
@@ -32,3 +50,4 @@ assert table_to_df.__doc__
 assert is_dataframe(data)
 assert not is_dataframe('oiabroibgao')
 assert is_weatherData.__doc__
+assert is_weatherData(test_df)
