e6e07a0fbf3b9827a8e574fee466fb069df6771e
Stmoller <stefan.moller@psy.lu.se>
2026-05-06 14:28:35 +0200

    Add check string function

--- learners/tdd_demo.py
+++ learners/tdd_demo.py
@@ -2,7 +2,8 @@
 
 
 def is_string(s):
-    return True
+    return isinstance(s,str)
+
 
 assert not is_string(314)
 
@@ -11,6 +12,8 @@ def int_to_roman(x):
     '''
     Convert a integer to a roman numeral.
     '''
+    return str(x)
 
 assert int_to_roman.__doc__
 assert is_string(int_to_roman(5))
+assert int_to_roman(1) == "I"
