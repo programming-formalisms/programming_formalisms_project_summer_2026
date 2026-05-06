271eb960eef91867d2284d5fc73710a683260bbb
Dakouri Kobri <dakouri.m.kobri@gmail.com>
2026-05-06 10:31:46 +0200

    Throws an exception when input is a string

--- learners/tdd_dakouri.py
+++ learners/tdd_dakouri.py
@@ -8,4 +8,12 @@ assert is_even.__doc__
 assert is_even(2)
 assert not is_even(3)
 
+has_thrown = False
+try:
+    is_even('john')
+except TypeError:
+    has_thrown = True
+
+assert has_thrown
+
 print()
