48b04bf09d026c1fe151a1342ac87cbbc11f75db
paubolivar <paubolivar@gmail.com>
2026-05-06 15:14:04 +0200

    assert arg is list in get_mean

--- learners/paulina/get_mean.py
+++ learners/paulina/get_mean.py
@@ -1,6 +1,8 @@
-def get_mean():
+def get_mean(l):
     """Compute the mean of list of numbers"""
-    pass
+    return True
+
 
 assert get_mean.__doc__
-assert get_mean(l)
+assert get_mean(1)
+assert isinstance(1, list)
