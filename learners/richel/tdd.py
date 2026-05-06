f60517b55824696bfe04fb1e904d7d351264bf41
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-05-06 09:32:03 +0200

    is_zero detects that 1 is not zero

--- learners/richel/tdd.py
+++ learners/richel/tdd.py
@@ -3,9 +3,13 @@ def is_zero(x):
     """"
     Determine if the input is zero. Return True if zero, False if non-zero. Raises an exception if input is not a number.
     """
-    return True
+    if (x == 0):
+        return True
+    else:
+        return False
 
 assert is_zero.__doc__
 assert is_zero(0) == True
-assert is_zero(0)
+assert is_zero(1) == False
+assert not is_zero(1)
 
