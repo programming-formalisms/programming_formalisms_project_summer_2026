baf34eb8e4ae7c43a594863cd0cc35662c3abdd1
remeseirogrp <chaitali.chakraborty@umu.se>
2026-05-06 10:57:12 +0200

    Version 0.2

--- learners/claudiachai5.4.py
+++ learners/claudiachai5.4.py
@@ -1,5 +1,13 @@
 def is_prime(x):
     """ This function tests if the value is prime. """
-    return True
+    if x % 2 == 0:
+        return False
+    else:
+        return True
+
+    
 assert is_prime.__doc__
 assert is_prime(1)
+#assert is_prime(2)
+assert is_prime(3)
+assert not is_prime(4) 
