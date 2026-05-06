ef2924b1fde8619081f5a631682498f26814de34
claudia.alleva <claudia.alleva@scilifelab.se>
2026-05-06 11:20:12 +0200

    Added divided by 3

--- learners/claudiachai5.4.py
+++ learners/claudiachai5.4.py
@@ -1,6 +1,10 @@
 def is_prime(x):
     """ This function tests if the value is prime. """
     if x % 2 == 0:
+        print ('This is even')
+        return False
+    elif x/3> 1 and x % 3 == 0:
+        print ('This can be divided by 3')
         return False
     else:
         return True
@@ -10,4 +14,6 @@ assert is_prime.__doc__
 assert is_prime(1)
 #assert is_prime(2)
 assert is_prime(3)
-assert not is_prime(4) 
+assert not is_prime(4)
+assert not is_prime(6)
+assert not is_prime(9)
