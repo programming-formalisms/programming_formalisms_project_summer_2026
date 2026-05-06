cc3af682c544be7b7a741415642d207372b780dc
Dakouri Kobri <dakouri.m.kobri@gmail.com>
2026-05-06 15:25:31 +0200

    assert input is a list

--- learners/paulina/get_mean.py
+++ learners/paulina/get_mean.py
@@ -1,8 +1,19 @@
 def get_mean(l):
     """Compute the mean of list of numbers"""
+    if not isinstance(l, list):
+        raise TypeError("'l' must be a list.")
     return True
 
-
 assert get_mean.__doc__
-assert get_mean(1)
-assert isinstance(1, list)
+
+has_thrown = False
+try:
+    assert get_mean(1)
+except TypeError:
+    has_thrown = True
+assert has_thrown
+    
+assert isinstance([1], list)
+
+assert get_mean([1]) == 1
+
