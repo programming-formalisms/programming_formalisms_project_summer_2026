3d60943e4b8689fed74c7d1293a131914cffb9c4
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-05-06 09:27:57 +0200

    Create 'is_zero' with documentation

---
+++ learners/richel/tdd.py
@@ -0,0 +1,9 @@
+
+def is_zero():
+    """"
+    Determine if the input is zero. Return True if zero, False if non-zero. Raises an exception if input is not a number.
+    """
+    pass
+
+assert is_zero.__doc__
+
