f1d137f1287d0eae833b0cfbc0647612b196ef76
paubolivar <paubolivar@gmail.com>
2026-05-06 15:30:53 +0200

    calculate mean in get_mean

--- learners/paulina/get_mean.py
+++ learners/paulina/get_mean.py
@@ -2,7 +2,8 @@ def get_mean(l):
     """Compute the mean of list of numbers"""
     if not isinstance(l, list):
         raise TypeError("'l' must be a list.")
-    return True
+    mean = sum(l) / len(l)
+    return mean
 
 assert get_mean.__doc__
 
@@ -16,4 +17,6 @@ assert has_thrown
 assert isinstance([1], list)
 
 assert get_mean([1]) == 1
+assert get_mean([2,2]) == 2
+assert get_mean([])   
 
