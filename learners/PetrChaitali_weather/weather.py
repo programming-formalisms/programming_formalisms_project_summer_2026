8af0fbd9404a1a550bfa103a6a0a71dcf8fe5497
stane <stanek.petr.19@gmail.com>
2026-05-06 17:52:25 +0200

    Added error classes, created city_filter(), adjusted is_wetherData func

--- learners/PetrChaitali_weather/weather.py
+++ learners/PetrChaitali_weather/weather.py
@@ -15,6 +15,35 @@ import pandas as pd
 # 6            Data id no. meaning data from:
 #                1=Uppsala, 2=Risinge, 3=Betna, 4=Linköping, 5=Stockholm, 6=Interpolated
 
+class MyError(Exception):
+    """Base class for custom errors"""
+    pass
+class InvalidCityError(MyError):
+    """Raised when city not defined/not part of dataset"""
+    pass
+class InvalidColumnTypeError(MyError):
+    """Raised when column is not present or do not contain expected data type"""
+    pass
+
+city_dict = {
+    "Uppsala"           :   1,
+    "Risinge"           :   2,
+    "Betna"             :   3,
+    "Linköping"         :   4,
+    "Stockholm"         :   5,
+    "Interpolated"      :   6
+}
+
+def city_filter(df: pd.DataFrame, city: str, city_dict: dict) -> pd.DataFrame:
+    """
+    Expects a dataframe with 7 columns, where the 7th column represent city/location 
+    Valid cities: Uppsala, Risinge, Betna, Linköping, Stockholm, Interpolated
+    Returns a dataframe with measurements only from the specified city
+    """
+    if city not in city_dict.keys():
+        raise InvalidCityError(f"City {city} is not defined in the dataset \nDefined cities are: {city_dict.keys()}")
+    return True
+
 def table_to_df(file):
     """ This function reads in the table. """
     data= pd.read_csv(file)
@@ -29,26 +58,22 @@ def is_weatherData(df):
     if df.shape[1] != 6:
         print("Dataframe must have 6 columns")
         return False
+    # NOTE: This could be wrapped in a for loop
     if not pd.api.types.is_integer_dtype(df.iloc[:, 0]):
-        print("Column 0 should be int")
-        return False
+        raise InvalidColumnTypeError(f"Column 1 is of type {df.iloc[:, 0].dtype}. Expected type: int")
     if not pd.api.types.is_integer_dtype(df.iloc[:, 1]):
-        print("Column 0 should be int")
-        return False
+        raise InvalidColumnTypeError(f"Column 2 is of type {df.iloc[:, 1].dtype}. Expected type: int")
     if not pd.api.types.is_integer_dtype(df.iloc[:, 2]):
-        print("Column 0 should be int")
-        return False
+        raise InvalidColumnTypeError(f"Column 3 is of type {df.iloc[:, 2].dtype}. Expected type: int")
     if not pd.api.types.is_float_dtype(df.iloc[:, 3]):
-        print("Column 0 should be int")
-        return False
+        raise InvalidColumnTypeError(f"Column 4 is of type {df.iloc[:, 3].dtype}. Expected type: float")
     if not pd.api.types.is_float_dtype(df.iloc[:, 4]):
-        print("Column 0 should be int")
-        return False
+        raise InvalidColumnTypeError(f"Column 5 is of type {df.iloc[:, 4].dtype}. Expected type: float")
     if (not pd.api.types.is_integer_dtype(df.iloc[:, 5]) or not df.iloc[:, 5].between(1,6).all()):
         print("Column 0 should be int and between 1 and 6.")
         return False
     else:
-        print ("This is the correct format.")
+#        print ("This is the correct format.")
         return True
 
 def is_dataframe(df):
