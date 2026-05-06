5898d4b92170111c2f6a45ee5a5eb7522f2a6519
mnyt-aqw <68369485+mnyt-aqw@users.noreply.github.com>
2026-05-06 11:40:26 +0200

    added doccumentation to file_exists as well as input varible.

--- learners/Marcus_and_Stefan.py
+++ learners/Marcus_and_Stefan.py
@@ -1,10 +1,18 @@
+def file_exists(file_path):
+    """
+    Check if input file exists
+    """
+    # Check if file exists
+    pass
+
 def do_experiment():
-  # Read the data
-  # Do the statistics
-  # Save the statistics results to file
-  assert file_exists("figure.png")
-  # Create the figure
-  # Save the figure to file
-  assert file_exists("statistics_results.txt")
+    # Read the data
+    # Do the statistics
+    # Save the statistics results to file
+    assert file_exists("figure.png")
+    # Create the figure
+    # Save the figure to file
+    assert file_exists("statistics_results.txt")
 
 assert do_experiment.__doc__
+assert file_exists.__doc__
