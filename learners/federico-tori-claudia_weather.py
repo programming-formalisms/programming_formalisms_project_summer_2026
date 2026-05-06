728b298db4e9fa184d76ce5164542a24a53b0880
torigiffin <victoria.giffin@uu.se>
2026-05-06 14:58:43 +0200

    made read_data and test for column names

--- learners/federico-tori-claudia_weather.py
+++ learners/federico-tori-claudia_weather.py
@@ -1,14 +1,19 @@
 import os
+import pandas as pd
 
 
 def file_exists(filename):
     """Check if file exists."""
     return os.path.isfile(filename)
 
+def read_data(filename):
+    content = pd.read_csv(filename, sep='\t')
+    assert column_names == 'Year, Month, Day, avg_temp, avg_temp_mod, location'
+    return content
 
 def do_experiment():
     # Read the data
-    assert read_data("data/uppsala_tm_1722-2022.dat")
+    assert not read_data("data/uppsala_tm_1722-2022.dat").empty
     # Do the statistics
     # Save the statistics results to file
     assert file_exists("statistics_results.txt")
