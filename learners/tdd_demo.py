503e86979f0cafdd19a273610fcc665ff1748fec
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-05-06 14:29:15 +0200

    Fix test

--- learners/tdd_demo.py
+++ learners/tdd_demo.py
@@ -12,7 +12,7 @@ def int_to_roman(x):
     '''
     Convert a integer to a roman numeral.
     '''
-    return str(x)
+    return "I"
 
 assert int_to_roman.__doc__
 assert is_string(int_to_roman(5))
