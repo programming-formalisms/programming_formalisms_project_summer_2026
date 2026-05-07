44dbd745f883739496a0a418df40dcaa1ef98126
Stmoller <stefan.moller@psy.lu.se>
2026-05-07 10:29:46 +0200

    Test corr values

---
+++ learners/stefan_code.py
@@ -0,0 +1,10 @@
+def check_corr(r):
+    if isinstance(r,float):
+        if -1.0 <= r <= 1.0:
+            return True
+        else:
+            raise ValueError("R needs to be between -1 and 1.")
+    else:
+        raise TypeError("R needs to be a float value.")
+    
+assert check_corr(1.0)
