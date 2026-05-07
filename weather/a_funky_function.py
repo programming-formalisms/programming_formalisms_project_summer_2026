18b97f353308ca93f7da1b0d0704b90743dbd96e
Eva Krämer <eva.kramer@umu.se>
2026-05-07 11:37:01 +0200

    Fixed code style

--- weather/a_funky_function.py
+++ weather/a_funky_function.py
@@ -1,24 +1,30 @@
-def funky(s, filename, num):
-    """_summary_
+import os
+
+def funky(s:str, filename:str, num:int):
+    """Make file with string s number (num) of times.
 
     Args:
-        x (_type_): _description_
-    """
 
-    f = open(filename, 'w')
-    for i in range(num):
-        f.write(s+'\n')
+        s (str): _description_
+        filename (str): _description_
+        num (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    f = open(filename, "w")
+    for _i in range(num):
+        f.writelines(s+"\n")
     f.close()
 
     return True
 
-
-test_filename = 'funky.txt'
-test_string = 'funky'
+test_filename = "funky.txt"
+test_string = "funky"
 test_num = 3
 assert funky(test_string, test_filename, test_num)
 
-import os
+
 assert os.path.exists(test_filename)
 with open(test_filename) as f:
     lines = f.readlines()
@@ -26,6 +32,5 @@ with open(test_filename) as f:
 assert len(lines)>0
 
 assert test_string in lines[0]
-print(lines)
 
 assert len(lines)==test_num
