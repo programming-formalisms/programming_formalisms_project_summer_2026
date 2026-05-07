ef9ed9fce4ccb16aa9605fdfce57a3a498610152
JorgeGarciaC <jorgegarciatdlc@hotmail.com>
2026-05-07 11:56:27 +0200

    Changes added

--- weather/jgc.py
+++ weather/jgc.py
@@ -1,13 +1,13 @@
-""" This script just reads a file and sotres it in f"""
+"""Script just reads a file and sotres it in f."""
 
 import os
-import typing
 
 file = "../data/uppsala_tm_1722-2022.txt"
 def read_file(your_file):
+    """Read a file"""
     with open(your_file) as f:
         f.read()
-        
+
 read_file(file)
 
 assert os.path.isfile(file)
