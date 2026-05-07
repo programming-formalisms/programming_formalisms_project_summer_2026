b166838bf138a4dd32f5471825a920172a6c60f1
Stmoller <stefan.moller@psy.lu.se>
2026-05-07 13:35:05 +0200

    Lint file

--- weather/stefan_code.py
+++ weather/stefan_code.py
@@ -1,15 +1,12 @@
 
 
 def check_corr(r):
-    '''
-    Check the value of a r values so it makes sense.
-    '''
+    """Check the value of a r values so it makes sense.
+    """
     if isinstance(r,float):
         if -1.0 <= r <= 1.0:
             return True
-        else:
-            raise ValueError("R needs to be between -1 and 1.")
-    else:
-        raise TypeError("R needs to be a float value.")
-    
+        raise ValueError("R needs to be between -1 and 1.")
+    raise TypeError("R needs to be a float value.")
+
 assert check_corr(1.0)
