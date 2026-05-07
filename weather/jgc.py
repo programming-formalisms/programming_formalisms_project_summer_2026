8ff2fbd60699b4a167644235efcd5073fbc021ee
JorgeGarciaC <jorgegarciatdlc@hotmail.com>
2026-05-07 11:36:37 +0200

    Add test file exist

--- weather/jgc.py
+++ weather/jgc.py
@@ -5,5 +5,5 @@ file = "../data/uppsala_tm_1722-2022.txt"
 with open(file) as f:
     print(f.read())
 
-#assert os._exists(file) # This does not work but is not giving error!
+assert os.path.isfile(file)
 assert isinstance(file,str)
