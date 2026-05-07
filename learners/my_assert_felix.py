fa2970f38603ddeedf52c9598b0ce058392b0ade
felix-falk <felix.falk@proton.me>
2026-05-07 09:41:30 +0200

    new update

--- learners/my_assert_felix.py
+++ learners/my_assert_felix.py
@@ -6,30 +6,40 @@ def do_experiment(dat_filename):
     import os
     assert os.path.isfile(filename) # Is the filename correct? 
     assert os.access(filename, os.R_OK) # Is the file readable?
-  # Read the data
+    # Read the data
     # Assert that the correct columns are in the file
     assert isinstance(numerator, float) # Assert that the data is correctly formatted
-  # Do the statistics
+    # Do the statistics
     # Assert that statistics have been created
-  # Save the statistics results to file
-  # Create the figure
-  # Save the figure to file
+    # Save the statistics results to file
+    # Create the figure
+    # Save the figure to file
     assert file_exists("figure.png") # Is the output figure created? 
     assert file_exists("statistics.txt") # Is the output statistic file created? 
 
 path = "/Users/felixfalk/programming_formalisms_project_summer_2026/data/uppsala_tm_1722-2022.dat"
 
-def read_file(filename):
-    import os
-    assert os.path.isfile(filename)
-    assert os.path.getsize(filename)
-    assert os.access(filename, os.R_OK)
-    
+#import pandas as pd
+#
+#def read_file(filename):
+#    """The read_file function reads the uppsala weather dat file, and returns the content."""
+#    import os
+#    assert os.path.isfile(filename)
+#    assert os.path.getsize(filename)
+#    assert os.access(filename, os.R_OK)
+#
+#    file = open(filename, "r")
+#    content = file.read()
+#    
+#    file.close()
+#
+#    return df
+#
+#read_file(path)
+#
+#df = pd.DataFrame(content)
 
-    file = open(filename, "r")
-    content = file.read()
+assert file_exists.__doc__
 
-    file.close()
-    return content
-
-read_file(path)
+assert file_exists("figure.png") # Is the output figure created?
+assert file_exists("statistics.txt") # Is the output statistics file created?
