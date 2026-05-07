4bd47efa41ae567c390e9a5b564f6647c7d0733b
mnyt-aqw <68369485+mnyt-aqw@users.noreply.github.com>
2026-05-07 08:07:25 +0200

    is_palindrome now checks if input is string, else it will return an error message.

--- learners/grp6_nicolas_marcus/functions.py
+++ learners/grp6_nicolas_marcus/functions.py
@@ -1,9 +1,21 @@
-def is_palindrome():
+def is_palindrome(string):
     """
     This function checks for if a strig is a palidrome.
     It accepts a string and terutns True if is palindrome, 
     else it returns False
     """
-    pass
 
-assert is_palindrome().__doc__
+    if not isinstance(string, str):
+        raise TypeError("Only strings are allowed")
+    else:
+        return True
+    
+
+assert is_palindrome.__doc__
+
+has_thrown = False
+try:
+    is_palindrome(1)
+except:
+    has_thrown = True
+assert has_thrown
