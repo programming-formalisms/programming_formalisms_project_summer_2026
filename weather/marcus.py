2f3a949d41a85775f38d33bdbe6b80095c8f3a71
mnyt-aqw <68369485+mnyt-aqw@users.noreply.github.com>
2026-05-07 11:59:35 +0200

    fixed even more linting issues

--- weather/marcus.py
+++ weather/marcus.py
@@ -1,54 +1,45 @@
 """Marcus test script. Lets fix it."""
 
 import os
+
 import matplotlib.pyplot as plt
 
 list_path = ["/Users/wenne/Documents/programing_formalisms/",
     "programming_formalisms_project_summer_2026/learners/grp6_nicolas_marcus/statistics_results.txt"]
 path_stats_out =  "".join(list_path)
-
 list_path_fig = ["/Users/wenne/Documents/programing_formalisms/",
                      "programming_formalisms_project_summer_2026/",
                      "learners/grp6_nicolas_marcus/figure.png"]
 path_fig_out = "".join(list_path_fig)
-
 def file_exists(file_path):
-    """
-    Check if input file exists.
+    """Check if input file exists.
 
     Returns True of file exists, else return False.
     """
-
     # Check if file exists
     return os.path.exists(file_path)
-   
+
 def generate_figure(data):
     """Generate plot."""
     # Plot data
     plt.figure(data)
     plt.savefig(path_fig_out)
-
 def clean_data():
-    """
-    Divide the data into relevant categories.
+    """Divide the data into relevant categories.
 
     Summer = Jun - Aug.
     Winter = Nov - Feb.
     """
     # Divide data into summer and winter
-
 def create_statisics_file(data, output_path):
-    """
-    Generate statiscs.
+    """Generate statiscs.
 
     The statistics will be performed by focus on the wealth
     data during the summer and winter, generate
     a data frame with P valve and regression data.
     """
-
     with open(output_path, "w") as fp:
         fp.write(data)
-
 def do_experiment():
     """Perform experiment."""
     # Read the data
@@ -59,7 +50,6 @@ def do_experiment():
     generate_figure(data)
     create_statisics_file(data, path_stats_out)
     # Save the figure to file
-
 do_experiment()
 assert do_experiment.__doc__
 assert file_exists.__doc__
