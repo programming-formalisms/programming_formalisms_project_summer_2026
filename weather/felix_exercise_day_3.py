c580ee75a1cc1729ee4fc90256be568d5ea1c858
stane <stanek.petr.19@gmail.com>
2026-05-07 21:00:50 +0200

    synced files from develop branch

--- weather/felix_exercise_day_3.py
+++ weather/felix_exercise_day_3.py
@@ -1,28 +1,50 @@
-# Felix do_experiment()
+"""Felix and Claudia do_experiment() file."""
 
-# do_experiment() # Should return a figure and a txt file
+import os
+
 
 def create_png(a):
     """Create a png file."""
+    with open(a, "w") as f:
+        f.writelines("This should be a figure!")
 
 assert create_png.__doc__
 
+def create_statistics(a):
+    """Create a txt file."""
+    with open(a, "w") as f:
+        f.writelines("This should contain statistics!")
+
+assert create_statistics.__doc__
+
 def file_exists(a):
     """Return True if a file exists, else False."""
-    import os
-    if (os.path.isfile(a) == True):
-        return True
-    return False
+    return bool(os.path.isfile(a))
 
 assert file_exists.__doc__
 
-def do_experiment(a):
+def do_experiment(a, b):
     """Take a .dat file as input, return a figure and a statistics file."""
-    # Create a statistics .txt file
-    # Create a png file
+    create_png(a) # Create a png file
+    create_statistics(b) # Create a statistics .txt file
 
-do_experiment(1)
+
+
+do_experiment("felix_figure.png", "felix_statistics.txt")
 
 assert do_experiment.__doc__
 assert file_exists("felix_figure.png")
 assert file_exists("felix_statistics.txt")
+
+with open("felix_figure.png") as f:
+    lines = f.readlines()
+assert len(lines)>0
+
+with open("felix_statistics.txt") as f:
+    lines = f.readlines()
+assert len(lines)>0
+
+with open("felix_statistics.txt") as f:
+    lines = f.readlines()
+
+assert "mean" in lines[0]
