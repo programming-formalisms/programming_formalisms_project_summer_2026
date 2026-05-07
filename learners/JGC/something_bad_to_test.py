904616e1c1dd96481dc5704419dd36b8d414a2e0
JorgeGarciaC <jorgegarciatdlc@hotmail.com>
2026-05-07 10:50:35 +0200

    Dumb failing test

--- learners/JGC/something_bad_to_test.py
+++ learners/JGC/something_bad_to_test.py
@@ -1,6 +1,10 @@
+# The last commit passed all the things so we will break it by adding this very large stupid line and breaking the assert 
+import os
+
 file = "../data/uppsala_tm_1722-2022.txt"
 
 with open(file) as f:
     print(f.read())
 
+assert os._exists(file) # This does not work
 assert type(file) == str 
