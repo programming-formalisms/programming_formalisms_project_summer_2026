efd4434d1a9ca8346ecc9a1e34315388900a0479
Krizia Pretell <kriziapretell@gmail.com>
2026-05-07 08:49:34 -0500

    fixed change kriz

--- weather/__init__.py
+++ weather/__init__.py
@@ -5,3 +5,69 @@ krizia_error = 1
 def my_function():
     """Return a greeeting message."""
     return "helo world"
+"""Main analysis module for the weather package."""
+
+import os.path
+
+from weather.anna import read_data as annas_read_data
+from weather.sven import create_figure as svens_create_figure
+from weather.sven import create_statistics_output as svens_create_statistics_output
+
+
+def check_file_exists(filename: str):
+    """Check if a file exists.
+
+    Return True if the file exists, False otherwise.
+    Raise TypeError if filename is not a string.
+
+    Args:
+        filename (str): Path to the file.
+
+    Returns:
+        bool: Whether the file exists.
+
+    """
+    if not isinstance(filename, str):
+        error_message = "Input 'filename' expected to be string."
+        raise TypeError(error_message)
+    return bool(os.path.exists(filename))
+
+
+def read_data():
+    """Read the weather data from file."""
+    return annas_read_data()
+
+
+def create_figure(data):
+    """Create the figure for the paper."""
+    return svens_create_figure(data)
+
+
+def create_statistics_output(data):
+    """Create a file with statistics results."""
+    return svens_create_statistics_output(data)
+
+
+def do_analysis():
+    """Do the analysis."""
+    data = read_data()
+    create_statistics_output(data)
+    create_figure(data)
+    print("Analysis done")  # noqa: T201
+
+
+# TODO(richelbilderbeek): move these to the 'test' folder.  # noqa: FIX002
+# https://github.com/programming-formalisms/programming_formalisms_project_summer_2026/issues/2
+
+do_analysis()
+assert os.path.isfile("figure.png")
+assert os.path.isfile("statistics_results.txt")
+assert check_file_exists("main.py")
+
+has_thrown = False
+try:
+    not check_file_exists(1231)
+except TypeError:
+    has_thrown = True
+
+assert has_thrown, "Expected TypeError was not raised"
