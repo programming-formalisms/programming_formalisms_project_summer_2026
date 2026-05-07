49ad1370f23ff5e52f5202cc0e61d1d344bd9274
Dakouri Kobri <dakouri.m.kobri@gmail.com>
2026-05-07 10:47:21 +0200

    Add 3 empty new lines in compute_mean

--- learners/dakouri/continuous_integration.py
+++ learners/dakouri/continuous_integration.py
@@ -1,3 +1,7 @@
 def compute_mean(data_list):
-    """Compute the mean from a list of numbeurs."""
-     return sum(data_list) / len(data_list)
+    """Compute the mean from a list of numbers."""
+    
+    
+    
+    
+    return sum(data_list) / len(data_list)
