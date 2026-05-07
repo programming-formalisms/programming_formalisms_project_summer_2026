edc6080923c34ba321307ac1ae173b96e371b723
stane <stanek.petr.19@gmail.com>
2026-05-07 11:50:09 +0200

    Even more of style fixes

--- weather/stane.py
+++ weather/stane.py
@@ -11,14 +11,16 @@ import numpy as np
 # Custom errors
 #--------------------------------------------------
 class MyError(Exception):
-    """
-    Base class for custom errors."""
+
+    """Base class for custom errors."""
+    
 class InvalidCityError(MyError):
-    """
-    Raised when city not defined/not part of dataset."""
+
+    """Raised when city not defined/not part of dataset."""
+
 class InvalidColumnTypeError(MyError):
-    """
-    Raised when column is not present or do not contain expected data type."""
+
+    """Raised when column is not present or do not contain expected data type."""
 
 
 #--------------------------------------------------
@@ -26,7 +28,7 @@ class InvalidColumnTypeError(MyError):
 #--------------------------------------------------
 
 # Weather data location
-weather_data = "../../data/uppsala_tm_1722-2022.dat"
+weather_data = "../data/uppsala_tm_1722-2022.dat"
 
 # City dictionary
 city_dict = {
