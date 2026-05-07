8b2f55b7d0f539c596409155d4843824bcceea94
JorgeGarciaC <jorgegarciatdlc@hotmail.com>
2026-05-07 14:30:26 +0200

    add things

--- weather/jgc.py
+++ weather/jgc.py
@@ -1,6 +1,7 @@
 """Script just reads a file and sotres it in f."""
 
 import os
+import pandas as pd
 
 file = "../data/uppsala_tm_1722-2022.txt"
 def read_file(your_file):
@@ -10,5 +11,15 @@ def read_file(your_file):
 
 read_file(file)
 
+data = "../data/uppsala_tm_1722-2022.dat"
+dt_clim = pd.read_csv(data)
+
+print(dt_clim)
+
+assert isinstance(statistic,float)
+
+assert isinstance(data,str)
+assert os.path.isfile(data)
+assert isinstance(dt_clim, pd.DataFrame)
 assert os.path.isfile(file)
 assert isinstance(file,str)
