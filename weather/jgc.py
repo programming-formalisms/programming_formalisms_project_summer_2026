dff6ee5f3d3c6943fe969cbdcad3a5ae387dc9d5
JorgeGarciaC <jorgegarciatdlc@hotmail.com>
2026-05-07 11:27:41 +0200

    Dumb test

---
+++ weather/jgc.py
@@ -0,0 +1,9 @@
+import os
+
+file = "../data/uppsala_tm_1722-2022.txt"
+
+with open(file) as f:
+    print(f.read())
+
+assert os._exists(file) # This does not work but is not giving error!
+assert type(file) == str 
