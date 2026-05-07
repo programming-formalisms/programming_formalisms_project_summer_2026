6f928d32841d8361f97f3f59d1d7248ab2d8d869
mnyt-aqw <68369485+mnyt-aqw@users.noreply.github.com>
2026-05-07 07:56:59 +0200

    Added doccumentation to is_palindrome function

--- learners/grp6_nicolas_marcus/functions.py
+++ learners/grp6_nicolas_marcus/functions.py
@@ -1,4 +1,9 @@
 def is_palindrome():
+    """
+    This function checks for if a strig is a palidrome.
+    It accepts a string and terutns True if is palindrome, 
+    else it returns False
+    """
     pass
 
 assert is_palindrome().__doc__
