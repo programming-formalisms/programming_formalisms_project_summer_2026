30e8eccaf4de81c870d8c809f7ea8180b6513be1
JorgeGarciaC <jorgegarciatdlc@hotmail.com>
2026-05-07 11:52:16 +0200

    Transform into function

--- weather/jgc.py
+++ weather/jgc.py
@@ -1,9 +1,14 @@
+""" This script just reads a file and sotres it in f"""
+
 import os
+import typing
 
 file = "../data/uppsala_tm_1722-2022.txt"
-
-with open(file) as f:
-    f.read()
+def read_file(your_file):
+    with open(your_file) as f:
+        f.read()
+        
+read_file(file)
 
 assert os.path.isfile(file)
 assert isinstance(file,str)
