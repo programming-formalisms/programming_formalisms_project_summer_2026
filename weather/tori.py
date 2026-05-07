c7a55de1de8339a95bae4c87da327feeef1ce743
torigiffin <victoria.giffin@uu.se>
2026-05-07 11:29:43 +0200

    Capitalizing and adding periods

--- weather/tori.py
+++ weather/tori.py
@@ -1,7 +1,7 @@
-"""Testing CICD Pipeline, example function"""
+"""Testing CICD Pipeline, example function."""
 
 def extract_stats(x):
-    """return x+2"""
+    """Return x+2."""
     return x+2
 
 extract_stats(1)
