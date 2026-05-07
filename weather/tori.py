5ba9016c14d533879c79bc6daec199da4c439c0e
torigiffin <victoria.giffin@uu.se>
2026-05-07 11:45:23 +0200

    this should work for develop, switching to tori

--- weather/tori.py
+++ weather/tori.py
@@ -1,48 +1,7 @@
-"""Weather project. Should read a file, calculate statistics and export a figure and a statistics file."""
+"""Testing CICD Pipeline, example function."""
 
-import os
-import pandas as pd
-import re
+def extract_stats(x):
+    """Return x+2."""
+    return x+2
 
-
-def file_exists(filename):
-    """Check if file exists."""
-    return os.path.isfile(filename)
-
-
-def read_data(filename):
-    content = []
-    with open(filename) as infile:
-        for line in infile:
-            line = re.sub("[ ]+", " ", line)
-            content.append(line.strip().split(" "))
-
-    content = pd.DataFrame(
-        content,
-        columns=["Year", "Month", "Day", "avg_temp", "avg_temp_mod", "location"],
-    )
-    return content
-
-assert list(read_data("data/uppsala_tm_1722-2022.dat").columns) == [
-    "Year",
-    "Month",
-    "Day",
-    "avg_temp",
-    "avg_temp_mod",
-    "location",
-]
-
-
-def do_experiment():
-    # Read the data
-    assert not read_data("data/uppsala_tm_1722-2022.dat").empty
-    # Do the statistics
-    assert not extract_stats(data).empty
-    # Save the statistics results to file
-    assert file_exists("statistics_results.txt")
-    # Create the figure
-    # Save the figure to file
-    # assert not file_exists("figure.png")
-
-
-do_experiment()
+extract_stats(1)
